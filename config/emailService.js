56734659ed59e0e4aa916722a163299db04dbeb2
fenil-dss <fenils@digitsoftsol.com>
2026-03-18 17:04:07 +0530

    nine commit in backend

--- config/emailService.js
+++ config/emailService.js
@@ -2,21 +2,21 @@ import sgMail from "@sendgrid/mail";
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-async function sendEmail(to, subject, html) {
+async function sendEmail(to, subject, text, html) {
   try {
-    if (!to || !subject || !html) {
+    if (!to || !subject || (!html && !text)) {
       throw new Error("Missing required email parameters");
     }
 
     const msg = {
       to,
-      from: process.env.EMAIL_USER, // verified email
+      from: process.env.EMAIL_USER,
       subject,
-      html,
+      text,   // optional
+      html,   // optional
     };
 
     await sgMail.send(msg);
-
     return { success: true };
   } catch (error) {
     console.error("SendGrid error:", error.response?.body || error.message);
--- config/sendEmail.js
+++ config/sendEmail.js
@@ -1,14 +1,8 @@
 import { sendEmail } from "./emailService.js";
 
-const sendEmailFun=async({sendTo, subject, text, html})=>{
-    const result = await sendEmail(sendTo, subject, text, html);
-    if(result.success){
-        return true;
-        // res.status(200).json({ message: 'Email sent successfully, messageId: result})
-    } else {
-        return false;
-        // res.status(500).json({ message: 'Failed to send email', error: result.error});
-    }
-}
+const sendEmailFun = async ({ sendTo, subject, html }) => {
+    const result = await sendEmail(sendTo, subject, html);
+    return result.success;
+};
 
 export default sendEmailFun;
